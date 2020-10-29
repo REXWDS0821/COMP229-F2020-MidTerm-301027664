4927acbe82cbb669291520ae6103d7b22d9d98e8
Rex <1780692450@qq.com>
2020-10-29 15:48:25 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://localhost/book229"
+  //"URI": "mongodb://localhost/books229"
+};
---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+// modules required for routing
+//Hao Wang, 301027664
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let book = require('../models/books');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+module.exports = router;
